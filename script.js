66132d50bd010270f530116bbf7fc137c9b7cb4d
AliNurisiahsiahi <alinurisiahsiahi@gmail.com>
2022-03-13 13:45:28 +0000

    100

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
+const searchBox = document.getElementById("search-box");
+const searchCount = document.getElementById("search-count");
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  //add an event listener here
+  searchBox.addEventListener("keyup", onSearchKeyUp);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodeContainer = document.getElementById("episode-list");
+  episodeContainer.innerHTML = "";
+
+  function formatSeriesAndEpisode(season, number) {
+    function padTheNumber(num) {
+      return num.toString().padStart(2, "0");
+    }
+    return `S${padTheNumber(season)}E${padTheNumber(number)}`;
+  }
+  episodeList.forEach((e) => {
+    const episode = document.createElement("div");
+    const heading = document.createElement("h3");
+    const summary = document.createElement("p");
+    const eImage = document.createElement("img");
+    eImage.src = e.image.medium;
+    // episodesImage.classList.add("img")
+    heading.innerText = `${e.name}-${formatSeriesAndEpisode(
+    e.season,
+    e.number
+  )}`;
+   
+    summary.innerHTML = e.summary;
+
+    episode.className = "episode";
+
+    episode.appendChild(heading);
+    episode.appendChild(summary);
+    episode.appendChild(eImage);
+    episodeContainer.appendChild(episode);
+  })
 }
 
+function onSearchKeyUp(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  const allEpisodes = getAllEpisodes();
+
+  const filteredEpisodes = allEpisodes.filter((e) => {
+    return e.name.toLowerCase().includes(searchTerm);
+  });
+  const filteredCount = filteredEpisodes.length;
+  const allCount = allEpisodes.length;
+
+  const countString = `Displaying ${filteredCount}/ ${allCount}`;
+  searchCount.innerText = countString;
+  makePageForEpisodes(filteredEpisodes)
+}
 window.onload = setup;
